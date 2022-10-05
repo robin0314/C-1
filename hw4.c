d57be83b2f58250a0fc2d8c900d78591e9839f5b
robin0314 <106158823+robin0314@users.noreply.github.com>
2022-10-06 01:09:39 +0900

    정수, 소수 판별
    정수, 소수 판별

---
+++ hw4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+	for (i = 0; i <= 5; i++) {
+		for (j = 1; j <= 5 - i; j++) {
+			printf(" ");
+		}
+		for (j = 1; j <= i * 2 - 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
